18c07113e64ebd341036d20a8bbb59ec371a82aa
Justin Smith <smithjn@g.cofc.edu>
2025-09-25 17:54:08 -0400

    Refactor: Make data classes immutable
    The `WeatherResponse.kt` file was updated to use `val` instead of `var` for all data class properties, making them immutable. Removed `@SerialName` annotations where the property name matched the JSON key.

--- app/src/main/java/com/example/weatherapp/model/WeatherResponse.kt
+++ app/src/main/java/com/example/weatherapp/model/WeatherResponse.kt
@@ -6,111 +6,84 @@ import kotlinx.serialization.SerialName
 
 @Serializable
 data class WeatherResponse(
-    @SerialName("cod")
-    var cod: String? = null,
-    @SerialName("message")
-    var message: String? = null,
-    @SerialName("cnt")
-    var cnt: Int? = null,
-    @SerialName("list")
-    var list: ArrayList<Forecast> = arrayListOf(),
-    @SerialName("city")
-    var city: City? = City()
+    val cod: String? = null,
+    val message: String? = null,
+    val cnt: Int? = null,
+    val list: ArrayList<Forecast> = arrayListOf(),
+    val city: City? = City()
 )
 
 @Serializable
 data class City(
-    @SerialName("id")
-    var id: Int? = null,
-    @SerialName("name")
-    var name: String? = null,
-    @SerialName("coord")
-    var coord: Coord? = Coord(),
-    @SerialName("country")
-    var country: String? = null,
-    @SerialName("population")
-    var population: Double? = null,
-    @SerialName("timezone")
-    var timezone: Int? = null,
-    @SerialName("sunrise")
-    var sunrise: Int? = null,
-    @SerialName("sunset")
-    var sunset: Int? = null
+    val id: Int? = null,
+    val name: String? = null,
+    val coord: Coord? = Coord(),
+    val country: String? = null,
+    val population: Double? = null,
+    val timezone: Int? = null,
+    val sunrise: Int? = null,
+    val sunset: Int? = null
 )
 
 @Serializable
 data class Clouds(
-    @SerialName("all")
-    var all: Int? = null
+    val all: Int? = null
 )
 
 @Serializable
 data class Coord(
-    @SerialName("lat")
-    var lat: Double? = null,
-    @SerialName("lon")
-    var lon: Double? = null
+    val lat: Double? = null,
+    val lon: Double? = null
 )
 
 @Serializable
 data class Forecast(
-    @SerialName("dt") var dt: Int? = null,
-    @SerialName("main") var main: Main? = Main(),
-    @SerialName("weather") var weather: ArrayList<Weather> = arrayListOf(),
-    @SerialName("clouds") var clouds: Clouds? = Clouds(),
-    @SerialName("wind") var wind: Wind? = Wind(),
-    @SerialName("visibility") var visibility: Int? = null,
-    @SerialName("pop") var pop: Double? = null,
-    @SerialName("sys") var sys: Sys? = Sys(),
-    @SerialName("dt_txt") var dtTxt: String? = null
+    val dt: Int? = null,
+    val main: Main? = Main(),
+    val weather: ArrayList<Weather> = arrayListOf(),
+    val clouds: Clouds? = Clouds(),
+    val wind: Wind? = Wind(),
+    val visibility: Int? = null,
+    val pop: Double? = null,
+    val sys: Sys? = Sys(),
+    @SerialName("dt_txt") val dtTxt: String? = null
 )
 
 @Serializable
 data class Main(
-    @SerialName("temp")
-    var temp: Double? = null,
+    val temp: Double? = null,
     @SerialName("feels_like")
-    var feelsLike: Double? = null,
+    val feelsLike: Double? = null,
     @SerialName("temp_min")
-    var tempMin: Double? = null,
+    val tempMin: Double? = null,
     @SerialName("temp_max")
-    var tempMax: Double? = null,
-    @SerialName("pressure")
-    var pressure: Int? = null,
+    val tempMax: Double? = null,
+    val pressure: Int? = null,
     @SerialName("sea_level")
-    var seaLevel: Int? = null,
+    val seaLevel: Int? = null,
     @SerialName("grnd_level")
-    var grndLevel: Int? = null,
-    @SerialName("humidity")
-    var humidity: Int? = null,
+    val grndLevel: Int? = null,
+    val humidity: Int? = null,
     @SerialName("temp_kf")
-    var tempKf: Double? = null
+    val tempKf: Double? = null
 )
 
 @Serializable
 data class Sys(
-    @SerialName("pod")
-    var pod: String? = null
+    val pod: String? = null
 )
 
 @Serializable
 data class Wind(
-    @SerialName("speed")
-    var speed: Double? = null,
-    @SerialName("deg")
-    var deg: Int? = null,
-    @SerialName("gust")
-    var gust: Double? = null
+    val speed: Double? = null,
+    val deg: Int? = null,
+    val gust: Double? = null
 )
 
 @Serializable
 data class Weather(
-    @SerialName("id")
-    var id: Int? = null,
-    @SerialName("main")
-    var main: String? = null,
-    @SerialName("description")
-    var description: String? = null,
-    @SerialName("icon")
-    var icon: String? = null
+    val id: Int? = null,
+    val main: String? = null,
+    val description: String? = null,
+    val icon: String? = null
 )
